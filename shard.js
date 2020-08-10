3c85f29a6f9bca8b653f8a12895c6cfe0d022fb8
londonmy10 <57810760+londonmy10@users.noreply.github.com>
2020-08-10 15:46:01 +0300

    Update shard.js

--- shard.js
+++ shard.js
@@ -1,14 +1,25 @@
-const { ShardingManager } = require(`discord.js`) 
-const ayarlar = require(`./ayarlar.json`)
- 
-const shards = new ShardingManager(`./bot.js`, { 
-    token : ayarlar.token, 
-    totalShards : 1 }); 
+const { ShardingManager } = require ('discord.js')
+const ayarlar = require ('./ayarlar.json')
+const Discord = require ('discord.js')
+const shards = new ShardingManager ('./bot.js', {//BURASI ANA DOSYANIZ DEGİİŞİR
 
-shards.on(`launch`, shard => { 
-console.log(`[${new Date().toString().split(` `,  5).join(` `)}] Shard: #${shard.id}`) }) 
+token : ayarlar.token,
 
-shards.on(`message`, (shard, msg) => { 
-console.log(`[${new Date().toString().split(` `, 5).join(` `)}] #${shard.id} | ${msg._eval} | ${msg._result}`) }); 
+totalShards : 2 //fireofeternity <3             
+
+});
+
+    const webhook = new Discord.WebhookClient("webhook id","webhook token")
+
+shards.on('launch', shard => {
+    webhook.send(`<a:baglaniyor:742313028933910591> **[Başlatılıyor]**${shard.id +1}. Shard Discorda yeniden bağlanmaya çalışıyor!`)
+    setTimeout(() => {
+  webhook.send(`<a:baglandi:742312990069751809> **[Başlatıldı]** ${shard.id +1}. Shard Discorda yeniden bağlandı!`)
+  }, 3000)
+})
+shards.on('message', (shard, msg) => {
+    console.log(`[${new Date().toString().split(" ", 5).join(" ")}] #${shard.id} | ${msg._eval} | ${msg._result}`);
+});
 
 shards.spawn()
+ 
