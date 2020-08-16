21f678a6ec76644ae9dd2d5a3252b03cccd148f8
londonmy10 <57810760+londonmy10@users.noreply.github.com>
2020-08-16 15:31:01 +0300

    Update shard.js

--- shard.js
+++ shard.js
@@ -9,7 +9,7 @@ totalShards : 1 //fireofeternity <3
 
 });
 
-    const webhook = new Discord.WebhookClient("webhook id","webhook token")
+    const webhook = new Discord.WebhookClient("742315341333397504","NOzl2n8n8rD1p92OBJQ3b9DbEPSWOtWcqjRtS8YT25f07iv_gh0fNJRIxLSvccRoJ39w")
 
 shards.on('launch', shard => {
     webhook.send(`<a:baglaniyor:742313028933910591> **[Başlatılıyor]**${shard.id +1}. Shard Discorda yeniden bağlanmaya çalışıyor!`)
